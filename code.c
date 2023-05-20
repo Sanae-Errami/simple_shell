a06e212ed99c135fd91870f7a8cdb0a1f1f76b07
Sanae Errami <erramisanae5@gmail.com>
2023-05-20 05:41:06 -0700

    task 0

---
+++ code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - calculate the sum of two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the sum
+ */
+int main(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
